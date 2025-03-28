9ebb013699f52dcc2ee7b4a02ca88836b59efd64
Aaron <aaronhustudent@163.com>
2025-03-28 21:16:58 +0800

    项目搬迁

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ElButton } from 'element-plus';
+import { ref } from 'vue';
+const msg = ref("你好，Vue！")
+const type = ref("primary")
+
+function switchColor()
+{
+  type.value = type.value == "primary"?"info":"primary"
+}
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <el-button :type="type" @click="switchColor" plain="true">{{msg}}</el-button>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
