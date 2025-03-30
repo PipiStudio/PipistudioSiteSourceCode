486552f6a7302f81d301db73883982c644b211b3
Aaron <aaronhustudent@163.com>
2025-03-30 20:18:38 +0800

    更新界面

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,81 @@
 <script setup>
-import { ElButton } from 'element-plus';
 import { ref } from 'vue';
-const msg = ref("你好，Vue！")
+import { Moon, Sunny} from '@element-plus/icons-vue'
+const darkMode = ref("dark")
 const type = ref("primary")
+const input = ref("别着急，Aaron还在修改网站！qwq")
 
 function switchColor()
 {
-  type.value = type.value == "primary"?"info":"primary"
+  type.value = type.value == "primary"?"danger":"primary"
 }
 </script>
 
 <template>
-  <header>
+  <html :class="darkMode">
+    <div class="common-layout">
+      <el-container>
 
-  </header>
+        <el-header>
+          <el-menu mode="horizontal">
+            <el-menu-item index="0">
+              <el-icon size="30"><Sunrise /></el-icon>
+            </el-menu-item>
+            
+            <el-menu-item index="1">项目</el-menu-item>
+            <el-menu-item index="2"><el-switch v-model="darkMode" active-value="dark" inactive-value="light" inline-prompt active-icon="Sunny" inactive-icon="Moon"></el-switch>
+            </el-menu-item>
+          </el-menu>
+        </el-header>
 
-  <main>
-    <el-button :type="type" @click="switchColor" plain="true">{{msg}}</el-button>
-  </main>
+        <el-main>
+          <el-row justify="center">
+            <el-card shadow="hover">
+              <p>{{ input }}</p>
+              <el-input v-model="input" style="width: 240px" placeholder="一个输入框..." type="textarea" rows="3"/>
+            </el-card>
+          </el-row>
+          <el-row justify="center">
+              <el-button :type="type" @click="switchColor" plain>{{ input }}</el-button>
+              <el-button :type="type" @click="switchColor" plain>{{ input }}</el-button>
+          </el-row>
+          <el-row justify="center">
+              <el-button :type="type" @click="switchColor" plain>{{ input }}</el-button>
+              <el-button :type="type" @click="switchColor" plain>{{ input }}</el-button>
+          </el-row>
+
+        </el-main>
+        <el-footer>
+          <el-empty description="别来无恙啊！" />
+        </el-footer>
+      </el-container>
+    </div>
+  </html>
 </template>
 
 <style scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
 
+
+html.dark .el-menu {
+  background-color: #1f1f1f;
+  color: #ffffff;
+}
+
+html.dark .el-menu-item {
+  color: #ffffff;
+}
+
+html.dark .el-menu-item:hover {
+  background-color: #333;
+}
+
+html.dark .el-menu-item.is-active {
+  background-color: #007bff;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import './assets/main.css'
 import App from './App.vue'
 
 
-createApp(App).use(ElementPlus)
-createApp(App).mount('#app')
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.use(ElementPlus)
+app.mount('#app')
